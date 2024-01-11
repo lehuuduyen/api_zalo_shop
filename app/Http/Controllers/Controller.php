6b90f212e8ba50f1626b6b906e2d97a0d7ec944d
lehuuduyen96 <lehuuduyen96@gmail.com>
2024-01-11 15:24:03 +0700

    add

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -504,7 +504,11 @@ class Controller extends BaseController
         if ($discount_total > $data['subtotal']) {
             $discount_total = $data['subtotal'];
         }
-
+        DB::connection('mysql_external')->table($this->_PRFIX_TABLE . '_postmeta')->where('post_id', $coupon->ID)->where('meta_key', 'usage_count')->update(
+            array(
+                'meta_value' => $usage_count+1
+            )
+        );
         return $discount_total;
     }
     public function timeFormat()

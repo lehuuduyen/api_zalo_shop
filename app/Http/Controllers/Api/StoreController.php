44980b4fce44e16cd08d6a03a4a58f7eeb7b7d70
admin <lehuuduyen96@gmail.com>
2024-03-13 23:52:54 +0700

    add

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -311,8 +311,13 @@ class StoreController extends Controller
         }
         $listUserChild = $listUserChild
         ->groupBy($this->_PRFIX_TABLE . '_users.ID',$this->_PRFIX_TABLE . '_users.user_login',$this->_PRFIX_TABLE . '_woo_history_user_commission.create_at')
-        ->get();
-
+        ->toSql();
+        
+        echo '<pre>';
+        print_r($listUserChild);
+        echo '</pre>';
+        die;
+        
 
 
         $tempIds =[];

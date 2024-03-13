570da75e047d3eeb7b73a189a1678d118a4df01c
lehuuduyen96 <lehuuduyen96@gmail.com>
2024-03-13 15:09:20 +0700

    ad

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -310,7 +310,7 @@ class StoreController extends Controller
             $listUserChild = $listUserChild->orderBy('ID', $request['order']);
         }
         $listUserChild = $listUserChild
-        ->groupBy($this->_PRFIX_TABLE . '_woo_history_user_commission.user_id')
+        ->groupBy($this->_PRFIX_TABLE . '_users.ID',$this->_PRFIX_TABLE . '_users.user_login')
         ->get();
         echo '<pre>';
         print_r($listUserChild);

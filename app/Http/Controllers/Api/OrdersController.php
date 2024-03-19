a61fade2e409da2a0e339ea862deca1d5feae0e9
admin <lehuuduyen96@gmail.com>
2024-03-19 22:12:34 +0700

    add

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -68,6 +68,11 @@ class OrdersController extends Controller
         ->select($this->_PRFIX_TABLE . '_wc_order_product_lookup.*', $this->_PRFIX_TABLE . '_posts.post_title')->get();
         $products = [];
         foreach ($ordersDetail as $key => $value) {
+            echo '<pre>';
+            print_r($value);
+            echo '</pre>';
+            die;
+            
             $phimId = $this->getPostMeta($value->order_id,'_film_selected');
             $phim = DB::table($this->_PRFIX_TABLE . '_films')->find($phimId);
 

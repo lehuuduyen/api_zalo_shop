2062d760ad795999e39726138a1d28a53ac1545a
lehuuduyen96 <lehuuduyen96@gmail.com>
2024-03-19 16:48:37 +0700

    ađ

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -39,9 +39,10 @@ class OrdersController extends Controller
 
                 $discount = DB::table($this->_PRFIX_TABLE . '_wc_orders_meta')->where('order_id', $order->id)->where('meta_key', 'discount')->first();
 
-                $orders[$key]->total_amount = $order->total_amount;
                 $orders[$key]->discount = ($discount)?$discount->meta_value:0;
-                $orders[$key]->total_price = $order->total_amount;
+                $orders[$key]->total_amount = $order->total_amount +$orders[$key]->discount;
+
+                $orders[$key]->total_price = $order->total_amount +$orders[$key]->discount;
 
                 $coupon = DB::table($this->_PRFIX_TABLE . '_wc_order_operational_data')->where('order_id', $order->id)->first();
                 if ($coupon) {

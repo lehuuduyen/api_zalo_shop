2ec47b2ec0c43fd2a2a041a7b46c570abf3c2be8
duyenleICD <duyenlh@icd-vn.com>
2024-04-03 15:07:13 +0700

    add

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -95,13 +95,14 @@ class OrdersController extends Controller
             $image = $this->getImage($value->product_id, $store);
             if(!$image){
                 $parentProduct = DB::connection('mysql_external')->table( $this->_PRFIX_TABLE .'_posts')->select('post_parent')->find($value->product_id);
-                echo $value->product_id;
-                echo '<pre>';
-                print_r($parentProduct);
-                echo '</pre>';
-                die;
+                
                 if($parentProduct){
                     $image = $this->getImage($parentProduct->post_parent, $store);
+                    echo $parentProduct->post_parent;
+                echo '<pre>';
+                print_r($image);
+                echo '</pre>';
+                die;
                 }
                     
             }

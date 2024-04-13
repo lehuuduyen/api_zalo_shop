92c1963fdd5f7a8849cfc6315256ce642fe2433c
duyenleICD <duyenlh@icd-vn.com>
2024-04-14 00:12:40 +0700

    add

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -644,13 +644,7 @@ class Controller extends BaseController
                         
                         $coupon_amount = $this->getPostMeta($coupon->ID, 'coupon_amount');
                         $coupon_type = $this->getPostMeta($coupon->ID, 'discount_type');
-                        echo '<pre>';
-                        print_r($coupon_discounted);
-
-                        echo "-----";
-                        print_r($coupon_amount);
-                        echo '</pre>';
-                        die;
+                        
                     DB::connection('mysql_external')->table($this->_PRFIX_TABLE . '_wc_order_coupon_lookup')->insertGetId(
                         array(
                             'order_id' => $postId,
@@ -663,7 +657,7 @@ class Controller extends BaseController
                         array(
                             'order_id' => $postId,
                             'order_item_type' => 'coupon',
-                            'order_item_name' => $data['used_coupon'],
+                            'order_item_name' => $coupon->post_title,
                         )
                     );
     

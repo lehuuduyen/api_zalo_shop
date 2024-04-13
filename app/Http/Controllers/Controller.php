8fef64af4be795ba57258c327dfd169cd156e758
duyenleICD <duyenlh@icd-vn.com>
2024-04-13 23:46:31 +0700

    ad

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1339,21 +1339,23 @@ class Controller extends BaseController
             $stock_count = $this->getPostMeta($item['id'], '_stock');
             $priceGoc = $this->getPostMeta($item['id'], '_regular_price');
             $price = $this->getPostMeta($item['id'], '_sale_price');
+            echo '<pre>';
+            print_r($priceGoc);
+            echo "---";
+            print_r($price);
+            echo '</pre>';
+            die;
             $_sale_price_dates_from = $this->getPostMeta($item['id'], '_sale_price_dates_from');
             $_sale_price_dates_to = $this->getPostMeta($item['id'], '_sale_price_dates_to');
             if ($price && $time >= $_sale_price_dates_from && $time <= $_sale_price_dates_to) {
                 $price = $price;
             } else if(!empty($price) && $price != $priceGoc){
                 $price = $price;
-                echo 123;
             }else {
                 $price = $priceGoc;
             }
 
-            echo '<pre>';
-            print_r($price);
-            echo '</pre>';
-            die;
+           
 
             $stockStatus = $this->getPostMeta($item['id'], '_stock_status');
             //checkcampaign

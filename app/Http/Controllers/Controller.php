ff56ec773a1dff2652b0594b75c9755d4e9b194e
duyenleICD <duyenlh@icd-vn.com>
2024-04-14 00:02:07 +0700

    add

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1209,7 +1209,7 @@ class Controller extends BaseController
                
                 $coupon_amount_total = $this->calculateCoupon($temp, [], true);
                 $listDetail[]=array(
-                    $detailCoupon => $coupon_amount_total
+                    "$detailCoupon" => $coupon_amount_total
                 );
                 $subtotal = $subtotal -$coupon_amount_total;
                 $discounted_price += $coupon_amount_total;

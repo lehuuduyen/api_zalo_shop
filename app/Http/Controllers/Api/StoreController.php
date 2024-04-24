43d16c81941ecd2c05be2c1df2d703c4f88804dd
admin <lehuuduyen96@gmail.com>
2024-04-24 22:16:34 +0700

    add

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -796,7 +796,7 @@ class StoreController extends Controller
                 $nameVongQuay[$value->id] =$value->name;
                 $pointVongQuay[$value->id] =$value->point;
             }
-            $selected_rate = randomWithWeight($rates);
+            $selected_rate = $this->randomWithWeight($rates);
 
             if($selected_rate){
                 $user = DB::connection('mysql_external')->table($this->_PRFIX_TABLE . '_usermeta')->updateOrInsert(

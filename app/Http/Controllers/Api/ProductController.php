c63cd9589d2209a29a6e9137ee2b2c2ee03a9da8
duyenleICD <duyenlh@icd-vn.com>
2024-05-09 14:22:07 +0700

    add

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -27,7 +27,7 @@ class ProductController extends Controller
         if($store->sdt != "77777777" ){
             $history = $this->getHistoryUser($userId);
             $point = $this->checkRank($history);
-            $discount = $point->discount;
+            $discount = ($point->discount)?$point->discount:0;
             $url_rank = $point->imageurl;
         }
         $products = DB::connection('mysql_external')->table($this->_PRFIX_TABLE . '_posts')->where('post_type', 'product')->where('post_status', 'publish')->orderBy('post_modified', 'DESC')->get();

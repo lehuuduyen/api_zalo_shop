b22b04b249ce6149764c178e417636a08380ef07
admin <lehuuduyen96@gmail.com>
2024-06-18 14:10:27 +0700

    add

--- app/Http/Controllers/Api/CouponsController.php
+++ app/Http/Controllers/Api/CouponsController.php
@@ -32,13 +32,14 @@ class CouponsController extends Controller
             $date_expires = $this->getPostMeta($val->ID,'date_expires');
             $usage_limit = $this->getPostMeta($val->ID,'usage_limit');
             $usage_count = $this->getPostMeta($val->ID,'usage_count');
-            if($usage_limit <= $usage_count){
-                continue;            
-            }
             echo '<pre>';
             print_r($val);
             echo '</pre>';
             die;
+            if($usage_limit <= $usage_count){
+                continue;            
+            }
+            
             
             if($customer_user){
                if($store->user_id == $customer_user){

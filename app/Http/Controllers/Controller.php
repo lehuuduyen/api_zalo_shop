a7a47d7036658d24db209767fcbe4f4dd0487619
admin <lehuuduyen96@gmail.com>
2024-06-20 16:50:39 +0700

    add

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -673,7 +673,11 @@ class Controller extends BaseController
             }
             //wp_wc_order_product_lookup
             $totalQuantity = array_sum($totalPriceDetails['quantity']);
-
+            echo '<pre>';
+            print_r($totalQuantity);
+            echo '</pre>';
+            die;
+            
             foreach ($totalPriceDetails['products_id'] as $key  => $productId) {
                 $products = DB::connection('mysql_external')->table($this->_PRFIX_TABLE . '_posts')->where('ID', $productId)->select('post_title')->first();
                 if (!$products) {

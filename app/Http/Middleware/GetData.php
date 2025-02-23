cbcd72563103964e9f125174d06b5778d7cae0ee
unknown <lehuuduyengit remote add origin https://github.com/hiengiangha3/thinkphp.git>
2025-02-23 16:08:25 +0700

    update

--- app/Http/Middleware/GetData.php
+++ app/Http/Middleware/GetData.php
@@ -26,35 +26,32 @@ class GetData extends Controller
 
             $timeNow = time();
             $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-            if(isset($data->sdt)){
-                if ($data) {
-                    if ($data->expired_in >= $timeNow || empty($data->expired_in)) {
-                        // Get the host (domain)
-                        $host = $_SERVER['HTTP_HOST'];
-    
-                        // Combine protocol and host to get the full domain
-                        $fullDomain = $protocol . \env('APP_URL_BACKEND');
-    
-                        $data->domain = $fullDomain;
-                        $request['data_reponse'] = $data;
-                        $this->connectDb($data->databaseStore);
-                    }
-                } else {
-    
+            if ($data) {
+                if ($data->expired_in >= $timeNow || empty($data->expired_in)) {
                     // Get the host (domain)
                     $host = $_SERVER['HTTP_HOST'];
-    
+
                     // Combine protocol and host to get the full domain
                     $fullDomain = $protocol . \env('APP_URL_BACKEND');
-                    $json = new stdClass();
-                    $json->domain = $fullDomain;
-                    $json->prefixTable = 'wp';
-    
-                    $request['data_reponse'] = $json;
+
+                    $data->domain = $fullDomain;
+                    $request['data_reponse'] = $data;
+                    $this->connectDb($data->databaseStore);
+                    return $this->returnError(new \stdClass, "Token không đúng hoặc hết hạn");
                 }
-    
+            } else {
+
+                // Get the host (domain)
+                $host = $_SERVER['HTTP_HOST'];
+
+                // Combine protocol and host to get the full domain
+                $fullDomain = $protocol . \env('APP_URL_BACKEND');
+                $json = new stdClass();
+                $json->domain = $fullDomain;
+                $json->prefixTable = 'wp';
+
+                $request['data_reponse'] = $json;
             }
-           
             return $next($request);
         } catch (\Throwable $th) {
             //throw $th;

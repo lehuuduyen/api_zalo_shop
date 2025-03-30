83a9b44f8f9d866a5e4c61ce55e0186b0606ead4
unknown <lehuuduyengit remote add origin https://github.com/hiengiangha3/thinkphp.git>
2025-03-30 11:16:55 +0700

    update

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -1308,6 +1308,6 @@ class StoreController extends Controller
     public function config()
     {
 
-        return $this->returnSuccess(true, '');
+        return $this->returnSuccess(false, 'Tính năng đặt hàng đang phát triển');
     }
 }
